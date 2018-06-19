84f8f564a18ad5eeec31cc317946fa4fb1f35413
elizabethadventurer <40374437+elizabethadventurer@users.noreply.github.com>
2018-06-18 20:09:26 -0500

    Mini Project 3

---
+++ Mini Project 3.py
@@ -0,0 +1,53 @@
+# Treasure-Seeker
+#High-Schooler version
+print("Hey stranger (fellow classmate) let's play a game!")
+initial = input("Would you like to play 10 questions? (The quicker to code version of 20 questions?)")
+
+if (initial == "Yes" or initial == "Sure" or initial == "Why not?" or initial == "Yeah" or initial == "yes"):
+    print("Then I'll start with the first question.")
+
+elif initial == "No" or initial == "Nah" or initial == "no":
+    print ("Then how about...we play a different game! Hmm..I suggest 'Test.py' or 'Practice2.py'...although I don't think either of them are complete...")
+    quit()
+
+age = int(input("How old are you? Please answer with only integers!"))
+
+grade = input("What grade are you going into?")
+
+if grade == "Senior" or grade == "senior" or grade == "12th grade" or grade == "12th":
+    print("Wow, already the last year of High School for you then...I hear it's difficult. Good luck!")
+if grade == "Junior" or grade == "junior" or grade == "11th" or grade == "11th grade":
+    print(Just 2 years! You can do it!)
+if grade.lower() == "sophmore":
+    print("No longer a fresh-man huh?")
+if grade.lower() == "freshman":
+    print("Good luck young grasshopper, you have much yet to experience.")
+answer1 = input("Who inspires you?")
+print(answer1, " is a good answer.")
+
+answer2 = input("What's your favorite drink?")
+print(answer2, "sounds good!")
+
+if answer2 == "Water" or answer2 == "water":
+    print("Water? That was very unoriginal. But hey at least you're healthy.")
+
+answer3 = input("Where would you like to go for a vacation getaway?"):
+    answer3andahalf = input("For how long?")
+    
+#answer4 =
+
+#answer5 =
+
+#answer6 =
+
+#answer7 =
+
+#answer8 =
+
+#answer9 =
+
+#answer10 =
+
+print("That's all folks!")
+print("Hasta la vista!")
+quit()
